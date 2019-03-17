f01e6e1fb96f4d9fe33c470b8d8c3d51e0e33de9
jero <jeroflat@gmail.com>
2019-03-17 21:21:27 +0300

    feat(app-middleware): use 'cors'

--- server/middleware/app-middleware.js
+++ server/middleware/app-middleware.js
@@ -1,9 +1,11 @@
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 
 // setup a global middleware
 module.exports = app => {
+  app.use(cors())
   app.use(morgan('combined'));
-  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 }; // we make the app exportable so it can be use by other apps
